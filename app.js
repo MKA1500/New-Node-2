01f72f1d0e107870939f5a92f119f2825aa18907
MKA1500 <maciejkabsch@gmail.com>
2018-05-03 11:38:14 +0200

    users inputs from CLI

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('command: ', command);
+console.log(process.argv);
+// for typing "node app.js list" etc.
+
+if (command === 'add') {
+  console.log('adding new note');
+} else if (command === 'list') {
+  console.log('listing all notes');
+} else if (command === 'read') {
+  console.log('reading note');
+} else {
+  console.log('command not recogized');
+}
