9b82e4a65e3a24c7417f5997c4a991f544c1e05c
MKA1500 <maciejkabsch@gmail.com>
2018-11-07 21:44:18 +0100

    using Yargs for command inputs

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+// var command = process.argv[2];
+var command = argv._[0];
 console.log('command: ', command);
 console.log(process.argv);
 console.log('Yargs', argv);
@@ -18,9 +19,11 @@ console.log('Yargs', argv);
 if (command === 'add') {
   notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('listing all notes');
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('reading note');
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
 } else {
   console.log('command not recogized');
 }
--- notes.js
+++ notes.js
@@ -5,7 +5,22 @@ var addNote = (title, body) => {
   // node app.js add --title="notes title" --body "this is a notes body"
 };
 
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
 module.exports = {
   // addNote: addNote
-  addNote
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
