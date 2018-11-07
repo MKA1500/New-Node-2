36e42d81020d2be738b180c2850caa7fd473a956
MKA1500 <maciejkabsch@gmail.com>
2018-11-07 20:40:17 +0100

    using Yargs for command inputs

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('command: ', command);
 console.log(process.argv);
+console.log('Yargs', argv);
+
+
 // for typing "node app.js list" etc.
 
 if (command === 'add') {
-  console.log('adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
   console.log('listing all notes');
 } else if (command === 'read') {
--- notes.js
+++ notes.js
@@ -1,6 +1,11 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+  // node app.js add --title="notes title" --body "this is a notes body"
+};
+
+module.exports = {
+  // addNote: addNote
+  addNote
 };
